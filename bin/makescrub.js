d68b7f3a00ed3e3c1b06ae5cbe069c27dc74c047
David Stolarsky <david.stolarsky@nytimes.com>
2017-01-25 17:50:46 -0500

    makescrub makes jpgs, is in PATH

--- bin/makescrub.js
+++ bin/makescrub.js
@@ -6,8 +6,10 @@ var fs = require('fs');
 const exec = require('child_process').exec;
 var tmp = require('tmp');
 const del = require('del');
+const mkdirp = require('mkdirp');
  
-var inputPath = argv._[0];
+var inputPath = argv._[0],
+    outputPath = argv._[1];
 
 
 var sequenceName = (() => {
@@ -17,61 +19,47 @@ var sequenceName = (() => {
 	return inputFilenameParts.slice(0, Math.max(1, inputFilenameParts.length-1)).join('.');
 })();
 
-tmp.dir(function _tempDirCreated(err, intermediateDir, cleanupCallback) {
-  if (err) throw err;
+// tmp.dir(function _tempDirCreated(err, intermediateDir, cleanupCallback) {
+//   if (err) throw err;
  
-  console.log("Storing intermediates in " + intermediateDir);
+//   console.log("Storing intermediates in " + intermediateDir);
 
-  function deleteIntermediates() {
-	del(path.join(intermediateDir, '*.jpg'), {/* allow outside CWD */ force:true}).then(paths => {
-	    // console.log('Deleted files and folders:\n', paths.join('\n'));
-	    cleanupCallback();
-	    console.log('Deleted intermediates');
-	}).catch((err) => {
-		console.error('Error deleting intermediates:');
-		console.error(err);
-	});
+ //  function deleteIntermediates() {
+	// del(path.join(intermediateDir, '*.jpg'), {/* allow outside CWD */ force:true}).then(paths => {
+	//     // console.log('Deleted files and folders:\n', paths.join('\n'));
+	//     cleanupCallback();
+	//     console.log('Deleted intermediates');
+	// }).catch((err) => {
+	// 	console.error('Error deleting intermediates:');
+	// 	console.error(err);
+	// });
   	
-  }
-  
-  exec('ffmpeg -i '+inputPath+' -f image2 '+path.join(intermediateDir,'frame%09d.jpg'), (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      deleteIntermediates();
-      return;
+ //  }
+var imageOutPath = path.join(outputPath, 'frames');
+mkdirp(imageOutPath, function (err) {
+    if (err) {
+    	console.error('Could not ensure directory ' + imageOutPath);
+    	process.exit(1);
     }
     else {
-	    // console.log(`stdout: ${stdout}`);
-	    // console.log(`stderr: ${stderr}`);
-	    exec(
-	    	path.join(__dirname, '../node_modules/.bin/img2data') + ' *.jpg',
-	    	{
-	    		cwd: intermediateDir,
-	    		maxBuffer: 200*1024*100
-	    	},
-	    	(error, stdout, stderr) => {
-	    		if(error) {
-	    			console.error(`img2data exec error: ${error}`);
-	    			console.log(`img2data stderr: ${stderr}`);
-	    			return;
-	    		}
-	    		else {
-	    			var outputCssPath = sequenceName + '.css';
-					fs.writeFile(outputCssPath, stdout, {encoding:'utf8'}, function(err) {
-						if(err) {
-							console.error(`error writing CSS: ${error}`);
-						}
-						else {
-							console.log('Created ' + outputCssPath);
-						}
-						deleteIntermediates();
-					});
-	    		}
-	    		
-	    	}
-	    );
+    	console.log('Ensured output directory ' + imageOutPath);
+    	console.log('Extracting frames from video...');
+	   	exec('ffmpeg -i '+inputPath+' -f image2 '+path.join(imageOutPath,'%09d.jpg'), (error, stdout, stderr) => {
+			if (error) {
+				console.error(`ffmpeg exec error: ${error}`);
+				// deleteIntermediates();
+				return;
+			}
+			else {
+				console.log('Wrote frames to ' + imageOutPath);
+				console.log('TODO create output JSON');
+			}
+
+		});
     }
 
-  });
 
 });
+
+
+// });
